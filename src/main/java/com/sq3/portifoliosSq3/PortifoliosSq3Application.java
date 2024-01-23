e5c1b98702f524118cf2d4ec0251657568a6b00a
Gabriel Lima <devgaboliverlima@gmail.com>
2024-01-23 13:14:15 -0300

    Projeto inicial backend e desenho do modelo de dados inicial

---
+++ src/main/java/com/sq3/portifoliosSq3/PortifoliosSq3Application.java
@@ -0,0 +1,13 @@
+package com.sq3.portifoliosSq3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortifoliosSq3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortifoliosSq3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sq3/portifoliosSq3/PortifoliosSq3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sq3.portifoliosSq3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortifoliosSq3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
