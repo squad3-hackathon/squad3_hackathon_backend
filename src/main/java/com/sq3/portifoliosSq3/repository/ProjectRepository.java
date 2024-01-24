74d64ad99bbb479cc2112a1a4c795455c0718fba
Gabriel Lima <devgaboliverlima@gmail.com>
2024-01-24 12:29:11 -0300

    Camada repository com os dados iniciais

---
+++ src/main/java/com/sq3/portifoliosSq3/repository/ProjectRepository.java
@@ -0,0 +1,7 @@
+package com.sq3.portifoliosSq3.repository;
+
+import com.sq3.portifoliosSq3.model.Project;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProjectRepository extends JpaRepository<Project, Long> {
+}
---
+++ src/main/java/com/sq3/portifoliosSq3/repository/TagRepository.java
@@ -0,0 +1,9 @@
+package com.sq3.portifoliosSq3.repository;
+
+import com.sq3.portifoliosSq3.model.Tag;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface TagRepository extends JpaRepository<Tag, Long> {
+}
