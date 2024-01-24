f12ad324cf0923643585a19ceb6b98b2063ba7a9
Gabriel Lima <devgaboliverlima@gmail.com>
2024-01-24 12:37:20 -0300

    Ajuste nos repositories

--- src/main/java/com/sq3/portifoliosSq3/repository/TagRepository.java
+++ src/main/java/com/sq3/portifoliosSq3/repository/TagRepository.java
@@ -3,7 +3,5 @@ package com.sq3.portifoliosSq3.repository;
 import com.sq3.portifoliosSq3.model.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface TagRepository extends JpaRepository<Tag, Long> {
 }
--- src/main/java/com/sq3/portifoliosSq3/repository/UserRepository.java
+++ src/main/java/com/sq3/portifoliosSq3/repository/UserRepository.java
@@ -2,8 +2,6 @@ package com.sq3.portifoliosSq3.repository;
 
 import com.sq3.portifoliosSq3.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 }
