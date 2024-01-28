945f3e0af37d23c15e843810c400af1ab03ebce6
Felippe Ferraz <felippeferrazjj@gmail.com>
2024-01-28 17:21:52 -0300

    refatoraÃacao de usuario e excepions

--- src/main/java/com/sq3/portifoliosSq3/exceptions/handler/ControllerAdvice.java
+++ src/main/java/com/sq3/portifoliosSq3/exceptions/handler/ControllerAdvice.java
@@ -7,12 +7,12 @@ import com.sq3.portifoliosSq3.exceptions.models.UserCreateException;
 import com.sq3.portifoliosSq3.exceptions.utils.ValidationError;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.nio.file.AccessDeniedException;
 import java.util.ArrayList;
 import java.util.List;
 
